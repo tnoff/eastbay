71fc755ca2c251034c0796b80876690b19a83cce
tnoff <1326564+tnoff@users.noreply.github.com>
2022-10-04 22:04:48 -0700

    remove extra app

--- eastbay_massage/settings.py
+++ eastbay_massage/settings.py
@@ -57,7 +57,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.staticfiles.finders.FileSystemFinder',
     # https://django-bootstrap-v5.readthedocs.io/en/latest/index.html
     'bootstrap5',
 ]
