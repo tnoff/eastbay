6fd0e96f375f0b1dec7475b896dcb9d5808af7d3
tnoff <1326564+tnoff@users.noreply.github.com>
2022-10-04 22:09:47 -0700

    add static root

--- eastbay_massage/settings.py
+++ eastbay_massage/settings.py
@@ -153,6 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
     '/var/www/static/',
