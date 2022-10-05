75aef9a79cc3051967bab0266fef4e565556e30f
tnoff <1326564+tnoff@users.noreply.github.com>
2022-10-04 22:15:25 -0700

    remove staticfile dirs

--- eastbay_massage/settings.py
+++ eastbay_massage/settings.py
@@ -154,9 +154,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [
-    '/var/www/static/',
-]
+#STATICFILES_DIRS = [
+#    '/var/www/static/',
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
