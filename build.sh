c02ccbfeb69f54313253c401c7db89a32e15b900
tnoff <1326564+tnoff@users.noreply.github.com>
2022-10-04 21:51:10 -0700

    add postgres db

--- build.sh
+++ build.sh
@@ -2,6 +2,4 @@
 set -o errexit
 
 pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
 python manage.py migrate
--- eastbay_massage/settings.py
+++ eastbay_massage/settings.py
@@ -97,12 +97,25 @@ WSGI_APPLICATION = 'eastbay_massage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'RENDER' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['POSTGRES_DATABASE'],
+            'USER': os.environ['POSTGRES_USER'],
+            'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+            'HOST': os.environ['POSTGRES_HOST'],
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
