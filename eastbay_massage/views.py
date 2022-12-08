23b8cc0864679e7ff708230ae55485c775b6533f
tnoff <1326564+tnoff@users.noreply.github.com>
2022-12-07 21:28:04 -0800

    simplify stuff back a bit

--- eastbay_massage/views.py
+++ eastbay_massage/views.py
@@ -2,27 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from twilio.twiml.voice_response import Play, VoiceResponse
 
-from eastbay_massage.settings import CONTACT_EMAIL, CONTACT_NUMBER, RENDER_EXTERNAL_HOSTNAME
+from eastbay_massage.settings import CONTACT_EMAIL, CONTACT_NUMBER
 
 CONTACT_INFO = {
   'contact_email': CONTACT_EMAIL,
   'contact_number': CONTACT_NUMBER,
+  'contact_number_no_space': CONTACT_NUMBER.replace('-', '').replace('(', '').replace(')', '').replace(' ', '')
 }
 
 def home(request):
   return render(request, 'home.html', CONTACT_INFO)
-
-def services(request):
-  return render(request, 'services.html', CONTACT_INFO) 
-
-def about(request):
-  return render(request, 'about.html', CONTACT_INFO) 
-
-def contact(request):
-  return render(request, 'contact.html', CONTACT_INFO) 
-
-def voicemail(request):
-  response = VoiceResponse()
-  response.play(f'https://{RENDER_EXTERNAL_HOSTNAME}/static/audio/voicemail.mp3')
-
-  return HttpResponse(response, content_type='text/xml')
